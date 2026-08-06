94131599572a60a5c0a59bcd54e6b222d9d89ea3
joaovictorcastro178 <joaovictorcastro178@gamil.com>
2026-08-05 21:55:42 -0300

    first commit

---
+++ src/dao/UsuarioDao.java
@@ -0,0 +1,32 @@
+package dao;
+import controller.ConnectionMySql;
+import model.UsuarioModel;
+import java.sql.Connection;
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class UsuarioDao {
+    private Connection connection;
+
+    public UsuarioDao(Connection connection) {
+        this.connection = new ConnectionMySql().getConnection();
+    }
+    
+    public void adicionar(UsuarioModel usuario){
+        String sql = "INSERT INTO usuário(nome,cpf,email,telefone,nascimento)VALUES(?,?,?,?,?)";
+        
+        try {
+            PreparedStatement ps = connection.prepareStatement(sql);
+            
+            ps.setString(1, usuario.getNome());
+            ps.setString(2, usuario.getCpf());
+            ps.setString(3, usuario.getEmail());
+            ps.setString(4, usuario.getTelefone());
+            ps.set(5, usuario.getNascimento());
+            
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar");
+            throw new RuntimeException(e);
+        }
+    }
+}
